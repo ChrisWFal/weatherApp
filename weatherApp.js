06eedb96c0a22fa8f8a0601dd3467ea2761ef847
Chris Falkenberg <falkenbergch@mullenmail.com>
2016-01-07 09:05:17 -0700

    added routers

--- weatherApp.js
+++ weatherApp.js
@@ -1,15 +1,32 @@
-var myApp = angular.module('weatherApp', []);
+var myApp = angular.module('weatherApp', ['ngRoute', 'ngResource']);
+
+weatherApp.config(function ($routeProvider)) {
+    $routeProvider
+
+        .when('/', {
+        templateurl: 'pages/home.html',
+        controller: 'homeController'
 
 
-myApp.controller('homeController', ['$scope', 'cityService'
-function($scope, cityService){
-    $scope.city = cityService.city;
-    $scope.$watch('city', function(){
-        cityService.city = $scope.city;
     })
+
+
+}
+
+
+myApp.controller('homeController', ['$scope', 'cityService',
+function ($scope, cityService) {
+        //    $scope.city = cityService.city;
+        //    $scope.$watch('city', function(){
+        //        cityService.city = $scope.city;
+        //   })
 }]);
 
-myApp.service('cityService', function(){
+myApp.controller('forecastController', ['$scope'], function ($scope, cityService) {
+
+})
+
+myApp.service('cityService', function () {
     this.city = "Denver, CO";
 
 })
