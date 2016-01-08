a7e52834d30e3cc8e9714de19d1d573d3b2c6c11
Chris Falkenberg <falkenbergch@mullenmail.com>
2016-01-08 09:48:31 -0700

    worked on the js files

--- weatherApp.js
+++ weatherApp.js
@@ -1,32 +1,45 @@
 var myApp = angular.module('weatherApp', ['ngRoute', 'ngResource']);
 
-weatherApp.config(function ($routeProvider)) {
+myApp.config(function ($routeProvider) {
     $routeProvider
 
         .when('/', {
-        templateurl: 'pages/home.html',
+        templateUrl: 'pages/home.html',
         controller: 'homeController'
 
 
     })
 
+    .when('/forecast', {
+        templateUrl: 'pages/forecast.html',
+        controller: 'forcastController'
 
-}
+
+    })
 
 
-myApp.controller('homeController', ['$scope', 'cityService',
-function ($scope, cityService) {
-        //    $scope.city = cityService.city;
-        //    $scope.$watch('city', function(){
-        //        cityService.city = $scope.city;
-        //   })
-}]);
+});
 
-myApp.controller('forecastController', ['$scope'], function ($scope, cityService) {
-
-})
 
 myApp.service('cityService', function () {
     this.city = "Denver, CO";
+
+
+    myApp.controller('homeController', ['$scope', 'cityService',
+function ($scope, cityService) {
+                $scope.city = cityService.city;
+
+                $scope.$watch('city', function () {
+                    cityService.city = $scope.city;
+
+                })
+           })
+    }]);
+
+myApp.controller('forecastController', ['$scope', 'cityService', function ($scope, cityService) {
+
+$scope.city = cityService.city;
+}])
+
 
 })
